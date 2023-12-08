d65750a9e74277a1e21808a32cbbac03f3d481c7
SvdNeo <sanjay.d@neoito.com>
2023-12-08 17:30:44 +0530

    Multiply numeric property values by 2

--- script.js
+++ script.js
@@ -86,3 +86,21 @@ let salaries = {
   }
   console.log(sum)
 
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === "number") {
+        obj[key] *= 2;
+      }
+    }
+  }
+  
+  multiplyNumeric(menu);
+  
+  console.log(menu);
+
