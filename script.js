dbd821170693cbdda1ecf884724de6c921ba01b8
SvdNeo <sanjay.d@neoito.com>
2023-12-08 16:55:53 +0530

    Sum object properties

--- script.js
+++ script.js
@@ -73,4 +73,16 @@ function isEmpty(obj){
     return true
 }
 
+//Sum object properties
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+    
+  }
+  let sum =0;
+  for(let key in salaries){
+    sum += salaries[key]
+  }
+  console.log(sum)
 
